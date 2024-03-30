3e1cf037b186de37ccd3ac68242330805304b8dd
sancaweb <sanca.snake@gmail.com>
2024-03-30 20:39:23 +0700

    preparing new template

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -40,8 +40,8 @@ class LoginController extends Controller
     /**
      * @override
      */
-    // public function username()
-    // {
-    //     return 'username';
-    // }
+    public function username()
+    {
+        return 'username';
+    }
 }
